8272ef17590a3952699b7091c743c1c114721989
刘成 <790809568@qq.com>
2018-01-25 21:14:40 +0800

    init

--- test-web/src/main/java/com/test/web/TestController.java
+++ test-web/src/main/java/com/test/web/TestController.java
@@ -1,6 +1,7 @@
 package com.test.web;
 
 import com.account.common.Account;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by apple on 2018/1/19.
  */
+@Slf4j
 @RestController
 public class TestController {
 
-    @GetMapping("/data")
+    @GetMapping("/index")
     public Object index(@AuthenticationPrincipal Account account){
+        log.info("data******************");
         return account;
     }
 }
